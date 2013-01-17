0600eb2201eaeb1c29b7c8d268e1b05124d4bf18
Mek Karpeles <mek@ark.com>
2013-01-16 18:25:54 -0800

    Very light scaffolding to encourage me to keep up with test cases

---
+++ openjournal/test/test_routes.py
@@ -0,0 +1,16 @@
+import unittest
+import requests
+
+class TestModules(unittest.TestCase):
+    def test_main(self):
+        import openjournal.main
+
+class TestRoutes(unittest.TestCase):
+
+    def test_home(self):
+        pass
+
+    def test_submit(self):
+        pass
+    
+    
