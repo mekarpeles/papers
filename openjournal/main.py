337c8a6cd758f07ce03fd7dd5fcd4ed748cf8c32
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-01-23 23:17:14 -0800

    Added routes for /item/# view and /upvote?pid= voting implemented.

--- openjournal/main.py
+++ openjournal/main.py
@@ -14,7 +14,8 @@ from lazydb.lazydb import Db
 import random
 
 urls = ('/submit', 'Submit',
-        '/item/?', 'Item',
+        '/item/([0-9]+)/?', 'Item',
+        '/upvote/?', 'Vote',
         '/', 'Index',
         '/404', 'NotFound',
         '(.*)', 'NotFound')
@@ -25,21 +26,37 @@ sessions = {'logged': False,
             'name': ''}
 app = waltz.setup.dancefloor(urls, globals(), env=env, sessions=sessions, autoreload=False)
 
+db = Db('db/openjournal')
+papers = lambda: db.get('papers')
+
 class Index:
     def GET(self):
-        papers = Db('db/openjournal').get('papers')
-        return render().index(papers)
+        return render().index(papers())
 
 class Vote:
-    def GET(self, pid=None):
+    def GET(self):
         """Research http://news.ycombinator.com/item?id=1781013 how
         hacker news voting works and emulate
         """
-        pass
+        i = web.input(pid=None)
+        if i.pid:
+            ps = papers()
+            
+            try:
+                ps[int(i.pid)]['votes'] += 1
+                db.put('papers', ps)
+            except IndexError:
+                return "No such items exists to vote on"
+            return render().index(ps)
 
 class Item:
     def GET(self, pid=None):
-        pass
+        if pid:            
+            try:
+                return papers()[int(pid)]
+            except IndexError:
+                return "No such item exists, id out of range"
+        raise web.seeother('/')
 
 class Submit:
     def GET(self):
