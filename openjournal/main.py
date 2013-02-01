12b3caa40061be1ee702fade254276505ef431b8
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-01-31 19:17:56 -0800

    Main.py now forks logic to routes (this should all have been committed together)

--- openjournal/main.py
+++ openjournal/main.py
@@ -10,87 +10,30 @@
 
 import waltz
 from waltz import web, track, session, render
-from lazydb.lazydb import Db
+import os
 import random
+import routes
+from lazydb import Db
 
-
-urls = ('/submit', 'Submit',
-        '/item/([0-9]+)/?', 'Item',
-        '/upvote/?', 'Vote',
-        '/', 'Index',
-        '/404', 'NotFound',
-        '(.*)', 'NotFound')
+urls = ('/submit/?', 'routes.item.Submit',
+        '/item/([0-9]+)/?', 'routes.item.Item',
+        '/upvote/?', 'routes.item.Vote',
+        '/login/?', 'routes.auth.Login',
+        '/logout/?', 'routes.auth.Logout',
+        '/x/?', 'routes.auth.Register',
+        '/404/?', 'routes.responses.NotFound',
+        '/admin/?', 'routes.admin.Analytics',
+        '/?', 'routes.index.Index',
+        '(.*)', 'routes.responses.NotFound')
 
 env = {'random': random}
 sessions = {'logged': False,
-            'uid': -1,
-            'name': ''}
-app = waltz.setup.dancefloor(urls, globals(), env=env, sessions=sessions, autoreload=False)
-
-JS = lambda msg: "<script type='text/javascript'>alert(\"%s\");</script>" % msg
-db = Db('db/openjournal')
-papers = lambda: db.get('papers')
-
-class Index:
-    def GET(self):
-        return render().index(papers())
-
-class Vote:
-    def GET(self):
-        """Research http://news.ycombinator.com/item?id=1781013 how
-        hacker news voting works and emulate
-        """
-        msg = None
-        i = web.input(pid=None)
-        if i.pid:
-            ps = papers()
-            if not session().logged:
-                msg = JS("Must be logged in to vote")
-            else:
-                try:
-                    ps[int(i.pid)]['votes'] += 1
-                    db.put('papers', ps)
-                except IndexError:
-                    return "No such items exists to vote on"
-            return render().index(ps, msg=msg)
-
-class Item:
-    def GET(self, pid=None):           
-        if pid:
-            try:
-                return papers()[int(pid)]
-            except IndexError:
-                return "No such item exists, id out of range"
-        raise web.seeother('/')
-
-class Submit:
-    def GET(self):
-        return render().submit()
-
-    def POST(self):
-        i = web.input(authors=None, url=None, title=None, year=None,
-                      enabled=False, submitter='', subtitle='',
-                      cite={'mla': '', 'apa': '', 'chicago': ''})
-        if i.authors:
-            clean_authors = []
-            dirty_authors = i.authors.split(',')
-            for author in dirty_authors:
-                opn = author.index(' (')
-                cls = author.index(') ')
-                name = author[:opn]
-                email = author[opn+2:cls]
-                inst = author[cls+1:]
-                clean_authors.append({'name': name,
-                                      'email': email,
-                                      'institution': inst})
-            i.authors = clean_authors
-        db = Db('db/openjournal')
-        db.append('papers', dict(i))
-        raise web.seeother('/')
-                  
-class NotFound:
-    def GET(self, err=None):
-        raise web.notfound('')
+            'authattempt': 0,
+            'uid': None,
+            'uname': ''}
+app = waltz.setup.dancefloor(urls, globals(), env=env, sessions=sessions,
+                             db='%s/db/waltz' % os.getcwd(),
+                             autoreload=False)
 
 if __name__ == "__main__":
     app.run()
