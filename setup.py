493a31692b0d593fecbfee470e8035e14dc87dd4
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-01-31 19:18:15 -0800

    Updated project dependencies for an easier build

---
+++ setup.py
@@ -0,0 +1,33 @@
+#-*- coding: utf-8 -*-
+
+"""
+    openjournal
+    ~~~~~~~~~~~
+"""
+
+from distutils.core import setup
+
+setup(
+    name='openjournal',
+    version='0.0.1',
+    url='http://github.com/mekarpeles/openjournal',
+    author='mek',
+    author_email='michael.karpeles@gmail.com',
+    packages=[
+        'openjournal',
+        'openjournal.subapps',
+        'openjournal.routes',
+        'openjournal.test'
+        ],
+    platforms='any',
+    scripts=[],
+    license='LICENSE',
+    install_requires=[
+        'waltz >= 0.1.62',
+        'lepl >= 5.1.3',
+        'pypdf',
+        'markdown'
+    ],
+    description="OpenJournal is a community for sharing academic papers.",
+    long_description=open('README.md').read(),
+)
