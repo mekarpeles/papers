1c4b1c6773edd14bb66df00debbc25fdc539a535
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-02-07 19:18:46 -0800

    Moved karma + vote blacklist funcs to utils

--- openjournal/utils.py
+++ openjournal/utils.py
@@ -1,6 +1,20 @@
 from datetime import datetime
+from waltz import User # should be moved elsewhere
 
 def str2datetime(s, fmt="%a %b %d %H:%M:%S %Y"):
     """Converts str timestamp to datetime"""
     return s if type(s) is datetime else \
         datetime.strptime(s, fmt)
+
+def canvote(u, pid):
+    return pid not in u['votes']
+
+def record_vote(yourname, submitter_name, pid, cid=None):
+    """XXX Todo: expose as web api/v1/vote and require nonce?"""
+    def inc_vote(user):
+        user['votes'].append(pid)
+        return user
+    submitter = User.get(submitter_name)
+    submitter['karma'] +=1
+    User.replace(submitter_name, submitter)
+    return User.update(yourname, func=inc_vote)
