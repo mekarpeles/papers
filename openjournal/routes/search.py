659efbbbcebdddbc7354bea56bd1c16b7175fbc7
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-05-26 13:26:08 -0700

    Paper.get() -> Paper.getall()

--- openjournal/routes/search.py
+++ openjournal/routes/search.py
@@ -7,7 +7,7 @@ class Search:
         i = web.input(search="")
         results = []
         if i.search:
-            papers = Paper.get()
+            papers = Paper.getall()
             matches = S().match(i.search)
             pids = [int(x['pid']) for x in matches]
             results = [p for p in papers if p['pid'] in pids]
