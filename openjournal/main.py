75f1612eacdf2d293b832436f6382ac2b245d5d9
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-05-26 23:09:35 -0700

    fixed web imports, made them relative to waltz

--- openjournal/main.py
+++ openjournal/main.py
@@ -9,13 +9,13 @@
     :license: BSD, see LICENSE for more details.
 """
 
-import waltz
-from waltz import web, track, User
-from web import wsgiserver
 import os
 import random
 import routes
 import datetime
+import waltz
+from waltz import web, User
+from web import wsgiserver
 from utils import str2datetime
 from lazydb import Db
 from configs.config import server
@@ -39,9 +39,9 @@ urls = ('/submit/?', 'routes.submit.Submit',
 env = {'random': random,
        'time': lambda x: web.datestr(str2datetime(x),
                                      now=datetime.datetime.utcnow()),
-       'karma': lambda: User.get(session()['uname'])['karma'],
+       'karma': lambda: User.get(waltz.session()['uname'])['karma'],
        'voted': lambda pid: int(pid) in \
-           User.get(session()['uname'])['votes'],
+           User.get(waltz.session()['uname'])['votes'],
        'join': lambda x, y: y.join(x)
        }
 sessions = {'logged': False,
