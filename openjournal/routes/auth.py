0d53baffb4b5232e020fbd15e617c3490ab778a5
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-05-27 22:45:46 -0700

    refactored authentication routes

--- openjournal/routes/auth.py
+++ openjournal/routes/auth.py
@@ -3,43 +3,21 @@
 """
     routes.auth
     ~~~~~~~~~~~
+
+    :copyright: (c) 2012 by Mek
+    :license: BSD, see LICENSE for more details.
 """
 
-import re
-import datetime
-from waltz import web, render, session, exponential_backoff, User
-from api.v1.user import Academic, ERR as AUTH_ERR
-
-def load_session(u, session):
-    """Constructs a dict of session variables for user u"""
-    session().update({'logged': True,
-                      'uname': u['username'],
-                      'email': u['email'],
-                      'created': u['created'],
-                      'bio': u['bio']
-                      })
-
-def invalidate_session(session):
-    session().update({'logged': False,
-                      'uname': '',
-                      'karma': 0,
-                      })
-    session().kill()
+from waltz import web, render, session
+from api.v1.user import Academic, AUTH_ERR
 
 def login(username, passwd):
-    """Logs a user in and sets their session data. Assumes
-    credentials have already validated via Academic.validates.
-    """
     try:
-        # assume user exists, try to fetch account
         u = Academic(username)
-    except AttributeError:
+        if u.authenticates(passwd):
+            return u.login()
+    except:
         return False
-    
-    if Academic.authenticates(u, passwd):
-        load_session(u, session)
-        return True
-    return False
 
 class Login:
     """Requires stateful exponential backoff to prevent rate limiting"""
@@ -49,51 +27,47 @@ class Login:
     def POST(self):
         """TODO: handle redir"""
         i = web.input(username='', passwd='', redir='/')
-        if not (i.username and i.passwd):
-            return render().auth.login(err=AUTH_ERR['missing_creds'])
+
         if not Academic.validates(i.username, i.passwd):
             return render().auth.login(err=AUTH_ERR['malformed_creds'])
+
         if login(i.username, i.passwd):
             raise web.seeother(i.redir)
+
         return render().auth.login(err=AUTH_ERR['wrong_creds'])
-        
+
 
 class Register:
     """Requires stateful exponential backoff to prevent rate limiting"""
     def GET(self):
+        if session().logged:
+            raise web.seeother('/')
         return render().auth.register()
 
     def POST(self):
-        def defusr():
-            return {'karma': 0,
-                    'comments': [],
-                    'votes': [],
-                    'posts': [],
-                    'created': datetime.datetime.utcnow(),
-                    'bio': '',
-                    'email': ''}
-
         i = web.input(username='', passwd='', redir='/')
 
-        if Academic.validates(i.username, i.passwd):
-            if login(i.username, i.passwd):
-                raise web.seeother(i.redir)
+        if session().logged:
+            raise web.seeother(i.redir)
 
-            try: # attempting registration if login
-                u = Academic.register(i.username, i.passwd, **defusr())
-                load_session(u, session)
-                raise web.seeother(i.redir)
-            except Academic.RegistrationException as e:
-                err = AUTH_ERR[str(e.message)]
-            else: # If there's an uncaught error, assuming missing args for auth
-                err = "Please enter all required fields"
+        if not Academic.validates(i.username, i.passwd):
+            return render().auth.login(err=AUTH_ERR['malformed_creds'])
+
+        # attempting login first
+        if login(i.username, i.passwd):
+            raise web.seeother(i.redir)
+
+        try:
+            u = Academic.register(i.username, i.passwd, **Academic.defaults())
+            u.login()
+            raise web.seeother(i.redir)
+        except Academic.RegistrationException as e:
+            err = AUTH_ERR[str(e.message)]
         return render().auth.register(err=err)
 
 class Logout:
     def GET(self):
         """Invalidate session, etc"""
-        i = web.input(redir='')
-        invalidate_session(session)
-        if i.redir:
-            raise web.seeother(i.redir)
-        raise web.seeother('/')
+        i = web.input(redir='/')
+        Academic.logout()
+        raise web.seeother(i.redir)
