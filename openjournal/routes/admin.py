307c28d83942718c4f0907f54b728f11ad8136cb
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-05-27 22:10:38 -0700

    pointing admin to waltz.modules.Analytics instead

--- openjournal/routes/admin.py
+++
@@ -1,7 +0,0 @@
-from waltz import db
-
-class Analytics:
-    def GET(self):
-        """This should be migrated to its own route module.
-        Also, Db call should be abstracted through an API"""
-        return db().get('analytics')
