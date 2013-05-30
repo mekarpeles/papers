6a328e614a8573e7412e09535b1d153bdaac36ea
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-05-30 02:57:29 -0700

    Bumping waltz dependency to v0.1.7

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     scripts=[],
     license='LICENSE',
     install_requires=[
-        'waltz >= 0.1.698',
+        'waltz >= 0.1.7',
         'paste >= 1.7.5.1',
         'lepl >= 5.1.3',
         'whoosh >= 2.4.1',
